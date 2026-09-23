179671a8ee073d076b974bb1d829d88dca6e3bd6
yogesh201sits <yogeshjamdade201@gmail.com>
2026-09-23 22:21:52 +0530

    feat added exports

--- packages/kafka/src/index.ts
+++ packages/kafka/src/index.ts
@@ -1,1 +1,20 @@
 export const KAFKA_BROKER = "localhost:9092";
+export { KafkaClient } from "./client";
+export type { KafkaClientConfig } from "./client";
+
+export { KafkaAdmin } from "./admin";
+
+export { KafkaProducer } from "./producer";
+export type { PublishMessage } from "./producer";
+
+export { KafkaConsumer } from "./consumer";
+export type {
+  ConsumedMessage,
+  MessageHandler,
+} from "./consumer";
+
+export {
+  TOPICS,
+  LOGS_RAW_PARTITIONS,
+  CONSUMER_GROUPS,
+} from "./topics";
